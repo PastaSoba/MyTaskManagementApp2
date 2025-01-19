e77b7634f09bb432c7e36a6fb9a064897aad53b7
Takeo Kurosawa <sanukinomiyatuko09@gmail.com>
2025-01-20 00:03:49 +0900

    _

--- program/controller/controller.py
+++ program/controller/controller.py
@@ -1,3 +1,18 @@
+from model.model import ProjectRoot
+from view.view import MainWindow
+
+
+
 class Controller:
-    def __init__(self, model, view):
+    """コントローラークラス。シングルトンパターンを適用している。
+    """
+    _instance = None
+
+    def __new__(cls, model:ProjectRoot, view:MainWindow):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self, model:ProjectRoot, view:MainWindow):
+        # ...existing code...
         pass
