7db89b1a707a576748bda6fb00c1791f0577baa4
Takeo Kurosawa <sanukinomiyatuko09@gmail.com>
2025-01-20 21:38:17 +0900

    _

--- program/view/CustomFrames/DetailFrame.py
+++ program/view/CustomFrames/DetailFrame.py
@@ -6,4 +6,10 @@ import tkinter.ttk as ttk
 class DetailFrame:
     def __init__(self, parent):
         self.frame = ttk.Frame(parent)
+        self._type_label = ttk.Label(self.frame, text="オブジェクトのタイプ") # testcode
+        self._name_label = ttk.Label(self.frame, text="オブジェクトの名前")   # testcode
+
+
+        self._type_label.pack(side=tk.TOP, fill="x", padx=10, pady=10) # testcode
+        self._name_label.pack(side=tk.TOP, fill="x", padx=10, pady=10) # testcode
         self.frame.pack(fill=tk.BOTH, expand=True)
