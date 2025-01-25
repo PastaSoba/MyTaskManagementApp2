2fa4029586f5c28d97124c3ffc40869f6b05e260
Takeo Kurosawa <sanukinomiyatuko09@gmail.com>
2025-01-26 00:34:44 +0900

    _

--- program/view/CustomFrames/DetailFrame.py
+++ program/view/CustomFrames/DetailFrame.py
@@ -15,19 +15,19 @@ class DetailFrame:
 class ProjectDetailFrame(DetailFrame):
     def __init__(self, parent: ttk.PanedWindow):
         super().__init__(parent)
-        self.name_entry = LabeledEntry(self.frame, 'Name')
-        self.due_entry = LabeledEntry(self.frame, 'Due')
+        self.name_entry      = LabeledEntry(self.frame, 'Name')
+        self.due_entry       = LabeledEntry(self.frame, 'Due')
         self.status_combobox = LabeledCombobox(self.frame, 'Status', [status.value for status in TaskStatus])
-        self.memo_entry = LabeledEntry(self.frame, 'Memo')
+        self.memo_entry      = LabeledEntry(self.frame, 'Memo')
 
 
 class TaskDetailFrame(DetailFrame):
     def __init__(self, parent: ttk.PanedWindow):
         super().__init__(parent)
-        self.name_entry = LabeledEntry(self.frame, 'Name')
-        self.due_entry = LabeledEntry(self.frame, 'Due')
-        self.status_combobox = LabeledCombobox(self.frame, 'Status', [status.value for status in TaskStatus])
-        self.memo_entry = LabeledEntry(self.frame, 'Memo')
-        self.assignee_entry = LabeledEntry(self.frame, 'Assignee')
-        self.estimation_entry = LabeledEntry(self.frame, 'Estimation')
+        self.name_entry        = LabeledEntry(self.frame, 'Name')
+        self.due_entry         = LabeledEntry(self.frame, 'Due')
+        self.status_combobox   = LabeledCombobox(self.frame, 'Status', [status.value for status in TaskStatus])
+        self.estimation_entry  = LabeledEntry(self.frame, 'Estimation')
         self.priority_combobox = LabeledCombobox(self.frame, 'Priority', [priority.value for priority in Priority])
+        self.assignee_entry    = LabeledEntry(self.frame, 'Assignee')
+        self.memo_entry        = LabeledEntry(self.frame, 'Memo')
